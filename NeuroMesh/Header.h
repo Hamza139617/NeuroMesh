df928c5d0cc88f72f3cd9e43af9b910421a7e43c
Hamza <hamza139617@gmail.com>
2026-09-11 15:28:41 +0500

    created the main structure

--- NeuroMesh/Header.h
+++ NeuroMesh/Header.h
@@ -1,2 +1,86 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+#include <cstdint>
+#include <climits>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+using namespace std;
 
+struct Layer {
+	int N;
+	int current_count;
+
+	int layerId;
+	Neuron* top_left;
+	Layer* next;
+	Layer* prev;
+};
+
+struct Neuron {
+	int id;
+	double weight;
+	Neuron* up;
+	Neuron* down;
+	Neuron* left;
+	Neuron* right;
+	Synapse* head_axon;
+};
+
+struct Synapse {
+	double weight;
+	bool is_active;
+	Neuron* target_neuron;
+	Synapse* next_synapse;
+};
+
+
+
+class NeuroMesh {
+private:
+	Layer* head_layer;
+	Layer* tail_layer;
+	int N;
+
+public:
+	NeuroMesh();
+
+	//==============creation================
+
+	void loadFromFile(const std::string& fileName);
+	void insertNeuron(int id, double weight);
+
+
+	//==============deleting=================
+
+	void deleteNeuron(int id);
+	void deleteLayer(int layerId);
+
+	//=============triggering================
+	void pruneNeuron(Neuron* n);
+	Neuron* mergeNeurons(Synapse* s);
+	Neuron* mergeColumns(Neuron* columnA_any, Neuron* columnB_any);
+	Layer* mergeLayers(Layer* a, Layer* b);
+	void removeEmptyLayer(Layer* l);
+
+	
+
+	//===============Printng/Exportng============
+	void printLayer(int layerid);
+	void exportMesh(const std::string& fileName);
+
+	void navigateMesh();
+
+	void printNeuron(int id);
+
+	//!!!!!!!!!!!!!!!!!searching!!!!!!!!!!!!!!!!!!!
+	Neuron* findNeuronById(int id);
+	Neuron* findNeuronByPosition(int layerId, int row, int col);
+
+
+	//!!!!!!!!!!!!!!!!!prapogation!!!!!!!!!!!!!!!!!
+	char forwardPropagate(char inputLetter);
+
+	void backwardPropagate(char targetLetter);
+};
